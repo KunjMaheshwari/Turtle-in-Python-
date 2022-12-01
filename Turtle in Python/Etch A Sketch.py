0be30cca0fdf7626bc367a03f822efbad2a616dc
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2022-12-01 15:06:18 +0530

    Add files via upload

---
+++ Turtle in Python/Etch A Sketch.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def move_counter_clockwise():
+    tim.left(10)
+
+
+def move_clockwise():
+    tim.right(10)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen = Screen()
+screen.listen()
+screen.onkey(key="W", fun=move_forward)
+screen.onkey(key="S", fun=move_backward)
+screen.onkey(key="A", fun=move_counter_clockwise)
+screen.onkey(key="D", fun=move_clockwise)
+screen.onkey(key="C", fun=clear)
+
+screen.exitonclick()
